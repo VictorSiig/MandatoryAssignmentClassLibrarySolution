fdff9d737165bb7746bdea30ddf736c5cb28e0eb
Victor <60875060+ViggoMayn@users.noreply.github.com>
2022-10-03 15:17:43 +0200

    Add project files.

---
+++ MandatoryAssignmentClassLibrary/FootballPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MandatoryAssignmentClassLibrary {
+    public class FootballPlayer {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public FootballPlayer() { }
+
+        public FootballPlayer(int id, string name, int age, int shirtNumber) {
+            Id = id;
+            Name = name;
+            Age = age;
+            ShirtNumber = shirtNumber;
+        }
+
+        public void ValidateId() {
+            if (Id < 1) {
+                throw new ArgumentOutOfRangeException("Id must be bigger than 0");
+            } else return;
+        }
+        public void ValidateName() {
+            if (Name.Length < 2) {
+                throw new ArgumentOutOfRangeException("Name must be longer than 1 character");
+            } else return;
+        }
+        public void ValidateAge() {
+            if (Age < 1) {
+                throw new ArgumentOutOfRangeException("Player must be older than 0 years old");
+            } else return;
+        }
+        public void ValidateShirtNumber() {
+            if (ShirtNumber < 1 || ShirtNumber > 99) {
+                throw new ArgumentOutOfRangeException("Shirt number must be between 1 and 99");
+            } else return;
+        }
+    }
+}
---
+++ MandatoryAssignmentClassLibraryTests/FootballPlayerTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MandatoryAssignmentClassLibrary;
+
+namespace MandatoryAssignmentClassLibrary.Tests {
+    [TestClass()]
+    public class FootballPlayerTests {
+
+        [TestMethod()]
+        public void FootballPlayerTest() {
+            FootballPlayer footballPlayer = new FootballPlayer(1, "Michael Laudrup", 56, 10);
+        }
+
+        [TestMethod()]
+        public void ValidateIdTest() {
+            FootballPlayer player = new FootballPlayer(1, "Michael Laudrup", 56, 10);
+            FootballPlayer invalidIdPlayer = new FootballPlayer(0, "Rasmus Lauge", 30, 9);
+            FootballPlayer negativeNrPlayer = new FootballPlayer(-10, "??", 101, 91);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidIdPlayer.ValidateId());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => negativeNrPlayer.ValidateId());
+        }
+
+        [TestMethod()]
+        public void ValidateNameTest() {
+            FootballPlayer player = new FootballPlayer(1, "Ib", 56, 10);
+            FootballPlayer invalidNamePlayer = new FootballPlayer(2, "Å", 30, 9);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidNamePlayer.ValidateName());
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest() {
+            FootballPlayer player = new FootballPlayer(1, "Michael Laudrup", 56, 10);
+            FootballPlayer invalidPlayer = new FootballPlayer(2, "Rasmus Lauge", 0, 9);
+            FootballPlayer negativeAgePlayer = new FootballPlayer(3, "??", -101, 91);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => invalidPlayer.ValidateAge());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => negativeAgePlayer.ValidateAge());
+        }
+
+        [TestMethod()]
+        public void ValidateShirtNumberTest() {
+            FootballPlayer player = new FootballPlayer(1, "Michael Laudrup", 56, 10);
+            FootballPlayer moreThanTopNumberPlayer = new FootballPlayer(2, "Rasmus Lauge", 0, 100);
+            FootballPlayer negativeNumberPlayer = new FootballPlayer(-3, "Peter", 18, -1);
+            FootballPlayer lessThanLowestNumberPlayer = new FootballPlayer(4, "Hans", 19, 0);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => moreThanTopNumberPlayer.ValidateShirtNumber());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => lessThanLowestNumberPlayer.ValidateShirtNumber());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => negativeNumberPlayer.ValidateShirtNumber());
+        }
+    }
+}
